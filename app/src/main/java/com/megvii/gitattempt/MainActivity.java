11cd4eaee9fd002fd8aeb5da5b10b564afaae06d
shuai <changshuai@megvii.com>
2018-08-20 17:00:59 +0800

    master 1 backup

--- app/src/main/java/com/megvii/gitattempt/MainActivity.java
+++ app/src/main/java/com/megvii/gitattempt/MainActivity.java
@@ -9,9 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello git test, 1st release");
 
-        int res = NewFeature.add(1, 2);
-        System.out.println("add feature added finish");
+        //release 1 
     }
 }
